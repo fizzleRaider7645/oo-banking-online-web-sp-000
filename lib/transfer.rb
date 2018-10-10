79638015adc1768dd178e41cd124277aadc6a90d
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-10 13:12:28 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,6 +2,8 @@ class Transfer
   attr_accessor :status, :amount
   attr_reader :sender, :receiver
   
+  @@transac_hist = []
+  
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
